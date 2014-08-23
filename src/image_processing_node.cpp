7e0f96faea308197474ffcfe65b5c6eed4608693
Luan Silveira <luansilveira.dp@gmail.com>
2014-08-23 04:31:49 -0300

    Correção do arquivo image_processing_node.cpp.

--- src/image_processing_node.cpp
+++ src/image_processing_node.cpp
@@ -1,1 +1,16 @@
+#include <ros/ros.h>
+#include <image_processing.h>
 
+using namespace dolphin_slam;
+
+int main(int argc, char **argv)
+
+{
+    ros::init(argc,argv,"image_processing");
+
+    ImageProcessing image_processing;
+
+    ros::spin();
+
+
+}
